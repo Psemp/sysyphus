a1580669613d5bdfd03518a26adc26051c165042
Pierre Sempere <pierre.sempere.01@gmail.com>
2024-03-15 16:08:19 +0100

    exceptions handling, purge, validation

--- sysyphus/models/boulder.py
+++ sysyphus/models/boulder.py
@@ -19,11 +19,15 @@ class Boulder:
             raise ConnectionError("The application has no access to the internet")
         self.sy_df = remote_load.get_remote_data(as_pd=True, use_json=True)
 
-    def make_search(self) -> pd.DataFrame:
+    def make_search(self, verbose_results: bool = True) -> pd.DataFrame:
         """
         Prompts the users for search parameters (the user can leave the prompts blank to ignore some).
         Uses the filters to get the meteorites matching the user's request. Returns the dataframe and
         keeps the search in memory to refine and request on in later stages.
+
+        Args:
+        - verbose_results : boolean, whether or not to return the dataframe of the results
+        (which is saved as object.selected_meteorites regardless). Default is True
 
         Returns:
         - result : a pd.DataFrame: A filtered DataFrame containing meteorite entries that match the specified
@@ -55,10 +59,37 @@ class Boulder:
         if result.__len__() > 0:
             self.selected_meteorites = result
 
-        return result
+        if verbose_results:
+            return result
 
     def validate_selection(self):
-        self.meteorite_objects = u_requests.make_meteorites(selected_meteorites=self.selected_meteorites)
+        if len(self.selected_meteorites) > 200:
+            warnings.warn(f"You have requested information on {len(self.selected_meteorites)} meteorites. "
+                          "This may stress the server and the app. Proceed?")
+
+            while True:
+                confirm = input("Enter N|No|Blank to abort, Y|Yes to proceed: ").lower().strip()
+
+                if confirm in ["n", "no", ""]:
+                    warnings.warn("Search halted. Reduce dataset size or allow potentially slow requests.")
+                    break
+                elif confirm in ["y", "yes"]:
+                    self.meteorite_objects = u_requests.make_meteorites(selected_meteorites=self.selected_meteorites)
+                    print("Meteorite objects created and ready to request.")
+                    return
+                else:
+                    print("Invalid input. Please enter Y|Yes to proceed or N|No|Blank to abort.")
+
+    def dump_search(self):
+        try:
+            del self.selected_meteorites
+        except AttributeError:
+            pass
+
+        try:
+            del self.meteorite_objects
+        except AttributeError:
+            pass
 
     def __repr__(self) -> str:
         return f"Boulder(sy_df={self.sy_df.shape})"
