05423726db7072d8ce034d2aeb6fd8e8049b8d28
Psemp <57976914+Psemp@users.noreply.github.com>
2024-03-19 18:52:57 +0100

    slight misshap, moving on to 0.0.2, no change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sysyphus",
-    version="0.0.1",
+    version="0.0.2",
     author="Pierre Sempéré",
     author_email="pierre.sempere.01@gmail.com",
     packages=find_packages(),
