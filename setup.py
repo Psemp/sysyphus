8b38a565bf8914ddb4ec1454f459f963604e021c
Psemp <57976914+Psemp@users.noreply.github.com>
2024-03-31 15:43:06 +0200

    moving setup to 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sysyphus",
-    version="0.1.0",
+    version="0.1.1",
     author="Pierre Sempéré",
     author_email="pierre.sempere.01@gmail.com",
     packages=find_packages(),
